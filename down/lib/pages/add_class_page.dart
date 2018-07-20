acb49aa4c9dfe6a3ab1b9fa23a611519eb717bcf
Peter Lee <peterdjlee@gmail.com>
2018-07-19 20:29:23 -0400

    add_class_page updated to ListView; textfield clearing bugs

--- down/lib/pages/add_class_page.dart
+++ down/lib/pages/add_class_page.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:down/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
 import 'dart:async';
 
@@ -11,7 +12,8 @@ class AddClassPage extends StatelessWidget {
   final TextEditingController _locationController = new TextEditingController();
   final TextEditingController _priceController = new TextEditingController();
   final TextEditingController _dateController = new TextEditingController();
-  final TextEditingController _timeController = new TextEditingController();
+  final TextEditingController _startTimeController = new TextEditingController();
+  final TextEditingController _endTimeController = new TextEditingController();
 
   Widget appBar(BuildContext context) {
     return AppBar(
@@ -58,7 +60,7 @@ class AddClassPage extends StatelessWidget {
       }
     }
 
-    Future<Null> _selectTime(BuildContext context) async {
+    Future<Null> _selectStartTime(BuildContext context) async {
       final TimeOfDay picked = await showTimePicker(
         context: context,
         initialTime: _time,
@@ -68,147 +70,252 @@ class AddClassPage extends StatelessWidget {
         _time = picked;
         String minute;
         if (_time.minute < 10) {
-          minute = '0$_time.minute';
+          minute = '0${_time.minute}';
         } else {
           minute = _time.minute.toString();
         }
 
         String period = _time.hour > 12 ? 'PM' : 'AM';
-        _timeController.text = '${_time.hourOfPeriod.toString()}:$minute $period';
+        _startTimeController.text = '${_time.hourOfPeriod.toString()}:$minute $period';
+      }
+    }
+
+    Future<Null> _selectEndTime(BuildContext context) async {
+      final TimeOfDay picked = await showTimePicker(
+        context: context,
+        initialTime: _time,
+      );
+
+      if (picked != null) {
+        _time = picked;
+        String minute;
+        if (_time.minute < 10) {
+          minute = '0${_time.minute}';
+        } else {
+          minute = _time.minute.toString();
+        }
+
+        String period = _time.hour > 12 ? 'PM' : 'AM';
+        _endTimeController.text = '${_time.hourOfPeriod.toString()}:$minute $period';
       }
     }
 
     final FocusNode _instructorFocusNode = new FocusNode();
     final FocusNode _locationFocusNode = new FocusNode();
     final FocusNode _priceFocusNode = new FocusNode();
+    final FocusNode _dateFocusNode = new FocusNode();
+    final FocusNode _startTimeFocusNode = new FocusNode();
+    final FocusNode _endTimeFocusNode = new FocusNode();
 
     return Scaffold(
+      // resizeToAvoidBottomPadding: false,
       appBar: appBar(context),
-      body: Container(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+      body: ListView(
+        shrinkWrap: true,
           children: <Widget>[
-            
-            PrimaryColorOverride(
-              color: downSalmon,
-              child: TextFormField(
-                controller: _titleController,
-                autofocus: true,
-                onFieldSubmitted: (String value) {
-                  FocusScope.of(context).requestFocus(_instructorFocusNode);
-                },
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
-                  labelText: 'Class Title',
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: PrimaryColorOverride(
+                color: downSalmon,
+                child: TextFormField(
+                  controller: _titleController,
+                  autofocus: true,
+                  textAlign: TextAlign.center,
+                  onFieldSubmitted: (String value) {
+                    FocusScope.of(context).requestFocus(_instructorFocusNode);
+                  },
+                  decoration: InputDecoration(
+                    // suffixIcon: IconButton(
+                    //   iconSize: 20.0,
+                    //   icon: Icon(FontAwesomeIcons.timesCircle),
+                    //   color: Colors.black26,
+                    //   onPressed: () {
+                    //     _titleController.clear();
+                    //   },
+                    // ),
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
+                    labelText: 'Class Title',
+                  ),
                 ),
               ),
             ),
 
-            SizedBox(height: 16.0),
-
-            PrimaryColorOverride(
-              color: downSalmon,
-              child: TextFormField(
-                controller: _instructorController,
-                focusNode: _instructorFocusNode,
-                onFieldSubmitted: (String value) {
-                  FocusScope.of(context).requestFocus(_locationFocusNode);
-                },
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
-                  labelText: 'Instructor',
-                ),
-              ),
-            ),
-
-            SizedBox(height: 16.0),
-
-            PrimaryColorOverride(
-              color: downSalmon,
-              child: TextFormField(
-                controller: _locationController,
-                focusNode: _locationFocusNode,
-                onFieldSubmitted: (String value) {
-                  FocusScope.of(context).requestFocus(_priceFocusNode);
-                },
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
-                  labelText: 'Location',
-                ),
-              ),
-            ),
-
-            SizedBox(height: 16.0),
-
-            Row(
-              children: <Widget>[
-                Flexible(
-                  child: PrimaryColorOverride(
-                    color: downSalmon,
-                    child: TextFormField(
-                      keyboardType: TextInputType.number,
-                      controller: _priceController,
-                      focusNode: _priceFocusNode,
-                      onFieldSubmitted: (String value) {
-                        _selectDate(context);
-                      },
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
-                        labelText: 'Price',
-                        prefixText: '\$',
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0, right: 16.0, bottom: 8.0),
+              child: PrimaryColorOverride(
+                color: downSalmon,
+                child: TextFormField(
+                  controller: _instructorController,
+                  focusNode: _instructorFocusNode,
+                  onFieldSubmitted: (String value) {
+                    FocusScope.of(context).requestFocus(_locationFocusNode);
+                  },
+                  decoration: InputDecoration(
+                    // suffixIcon: IconButton(
+                    //   iconSize: 20.0,
+                    //   icon: Icon(FontAwesomeIcons.timesCircle),
+                    //   color: Colors.black26,
+                    //   onPressed: () {
+                    //     _instructorController.clear();
+                    //   },
+                    // ),
+                    icon: Icon(
+                      Icons.person_outline,
+                      color: Colors.black,
                       ),
+                    labelText: 'Instructor',
+                  ),
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0, right:16.0, top: 8.0, bottom: 8.0),
+              child: PrimaryColorOverride(
+                color: downSalmon,
+                child: TextFormField(
+                  controller: _locationController,
+                  focusNode: _locationFocusNode,
+                  onFieldSubmitted: (String value) {
+                    FocusScope.of(context).requestFocus(_priceFocusNode);
+                  },
+                  decoration: InputDecoration(
+                    // suffixIcon: IconButton(
+                    //   iconSize: 20.0,
+                    //   icon: Icon(FontAwesomeIcons.timesCircle),
+                    //   color: Colors.black26,
+                    //   onPressed: () {
+                    //     _locationController.clear();
+                    //   },
+                    // ),
+                    icon: Icon(
+                      Icons.location_on,
+                      color: Colors.black,
+                      ),
+                    labelText: 'Location',
+                  ),
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0, right:16.0, top: 8.0, bottom: 8.0),
+              child: PrimaryColorOverride(
+                  color: downSalmon,
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    controller: _priceController,
+                    focusNode: _priceFocusNode,
+                    onFieldSubmitted: (String value) {
+                      FocusScope.of(context).requestFocus(_dateFocusNode);
+                    },
+                    decoration: InputDecoration(
+                      // suffixIcon: IconButton(
+                      //   iconSize: 20.0,
+                      //   icon: Icon(FontAwesomeIcons.timesCircle),
+                      //   color: Colors.black26,
+                      //   onPressed: () {
+                      //     _priceController.clear();
+                      //   },
+                      // ),
+                      icon: Icon(
+                        Icons.monetization_on,
+                        color: Colors.black,
+                      ),
+                      labelText: 'Price',
+                      prefixText: '\$',
                     ),
                   ),
                 ),
+            ),
 
-                SizedBox(width: 16.0),
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0, right:16.0, top: 8.0, bottom: 8.0),
+              child: PrimaryColorOverride(
+                color: downSalmon,
+                child: TextFormField(
+                  controller: _dateController,
+                  focusNode: _dateFocusNode,
+                  onFieldSubmitted: (String value) {
+                    FocusScope.of(context).requestFocus(_startTimeFocusNode);
+                  },
+                  decoration: InputDecoration(
+                    helperStyle: TextStyle(
+                      fontSize: 12.0
+                    ),
+                    hintText: 'mm/dd/yy',
+                    helperText: '*Format = mm/dd/yy',
+                    icon: Icon(
+                      Icons.calendar_today,
+                      color: Colors.black,
+                    ),
+                    labelText: 'Date',
+                  ),
+                ),
+              ),
+            ),
 
-                Flexible(
-                  child: PrimaryColorOverride(
-                    color: downSalmon,
-                    child: TextFormField(
-                      controller: _dateController,
-                      decoration: InputDecoration(
-                        suffixIcon: IconButton(
-                          icon: Icon(Icons.calendar_today),
-                          onPressed: () {
-                            _selectDate(context);
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0, right:16.0, top: 8.0, bottom: 8.0),
+              child: Row(
+                children: <Widget>[
+                  Flexible(
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: PrimaryColorOverride(
+                        color: downSalmon,
+                        child: TextFormField(
+                          controller: _startTimeController,
+                          focusNode: _startTimeFocusNode,
+                          onFieldSubmitted: (String value) {
+                            FocusScope.of(context).requestFocus(_endTimeFocusNode);
                           },
-                          
+                          decoration: InputDecoration(
+                            helperStyle: TextStyle(
+                              fontSize: 12.0
+                            ),
+                            hintText: 'hh:mm',
+                            helperText: '*Format = hh:mm',
+                            icon: Icon(
+                              Icons.access_time,
+                              color: Colors.black,
+                           ),
+                           labelText: 'Start Time',
+                         ),
                         ),
-                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
-                        labelText: 'Date',
                       ),
                     ),
                   ),
-                ),
 
-                SizedBox(width: 16.0),
-
-                Flexible(
-                  child: PrimaryColorOverride(
-                    color: downSalmon,
-                    child: TextFormField(
-                      controller: _timeController,
-                      decoration: InputDecoration(
-                        suffixIcon: IconButton(
-                          icon: Icon(Icons.access_time),
-                          onPressed: () {
-                            _selectTime(context);
-                          }
+                  Flexible(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: PrimaryColorOverride(
+                        color: downSalmon,
+                        child: TextFormField(
+                          controller: _endTimeController,
+                          focusNode: _endTimeFocusNode,
+                          decoration: InputDecoration(
+                            helperStyle: TextStyle(
+                              fontSize: 12.0
+                            ),
+                            hintText: 'hh:mm',
+                            helperText: '*Format = hh:mm',
+                            icon: Icon(
+                              Icons.access_time,
+                              color: Colors.black,
+                           ),
+                           labelText: 'End Time',
+                         ),
                         ),
-                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
-                        labelText: 'Time',
                       ),
                     ),
                   ),
-                ),
-              ],
+                ]
+              ),
             ),
           ],
         ),
-      ),
       bottomNavigationBar: ButtonTheme(
               minWidth: double.infinity,
               height: 64.0,
@@ -229,18 +336,21 @@ class AddClassPage extends StatelessWidget {
                   String location = _locationController.text;
                   int price = int.parse(_priceController.text);
                   String date = _dateController.text;
-                  String time = _timeController.text;
+                  String startTime = _startTimeController.text;
+                  String endTime = _endTimeController.text;
                   String documentID = title.toLowerCase().replaceAll(' ', '')
                   + instructor.toLowerCase().replaceAll(' ', '')
                   + location.toLowerCase().replaceAll(' ', '')
                   + price.toString();
 
-                  org = {'title' : title, 
+                  org = {
+                  'title' : title, 
                   'instructor' : instructor,
                   'location' : location, 
                   'price' : price,
                   'date' : date,
-                  'time' : time
+                  'startTime' : startTime,
+                  'endTime' : endTime,
                   };
 
                   Firestore.instance.collection('classes').document(documentID).setData(org);
